27daf189400160931938fd72783d57987e00a6c8
codejunk1e <emeka14real@gmail.com>
2021-07-29 09:23:47 -0700

    Updates to header file

--- holberton.h
+++ holberton.h
@@ -1,1 +1,23 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char );
 int _printf(const char *format, ...);
+unsigned int handl_buf(char *, char , unsigned int );
+
+#endif  /* _HOLBERTON_H */
